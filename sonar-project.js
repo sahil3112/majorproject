cdcb6b4f68bb48fb0d0baf40d2d61cc83ca92c01
sahil gupta <43255158+sahil3112@users.noreply.github.com>
2021-11-14 22:50:32 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,6 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
      sonarqubeScanner({
        serverUrl: 'http://192.168.56.101:9000',
+       token : "e106af7b98804fa838097bb0abcababa7cd56182",
        options : {
        'sonar.sources': '.',
        //'sonar.inclusions' : '.' // Entry point of your code
